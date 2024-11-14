244b7207e04866e7fddef9fe50356cae2c264dd0
enfmarinho <eduardonestormarinho228@gmail.com>
2024-11-14 20:37:36 -0300

    Updating which-key config to v3

--- lua/default/plugins/general/which-key.lua
+++ lua/default/plugins/general/which-key.lua
@@ -7,6 +7,8 @@ return {
 		vim.o.timeoutlen = 300
 	end,
 	opts = {
+		preset = "modern",
+		notify = true,
 		plugins = {
 			marks = true, -- shows a list of your marks on ' and `
 			registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -24,31 +26,39 @@ return {
 				g = false, -- bindings for prefixed with g
 			},
 		},
-		operators = { gc = "Comments" },
-		key_labels = {
-			["<space>"] = "SPC",
-			["<cr>"] = "CR",
-			["<tab>"] = "TAB",
-		},
-		motions = {
-			count = true,
-		},
+		colors = true,
 		icons = {
 			breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
 			separator = "»", -- symbol used between a key and it's label
 			group = "+", -- symbol prepended to a group
-		},
-		popup_mappings = {
-			-- scroll_down = "<c-d>", -- binding to scroll down inside the popup
-			-- scroll_up = "<c-u>", -- binding to scroll up inside the popup
-		},
-		window = {
-			border = "none", -- none, single, double, shadow
-			position = "bottom", -- bottom, top
-			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-			padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-			winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-			zindex = 1000, -- positive value to position WhichKey above other floating windows.
+			Up = " ",
+			Down = " ",
+			Left = " ",
+			Right = " ",
+			C = "󰘴 ",
+			M = "󰘵 ",
+			D = "󰘳 ",
+			S = "󰘶 ",
+			CR = "󰌑 ",
+			Esc = "󱊷 ",
+			ScrollWheelDown = "󱕐 ",
+			ScrollWheelUp = "󱕑 ",
+			NL = "󰌑 ",
+			BS = "󰁮",
+			Space = "󱁐 ",
+			Tab = "󰌒 ",
+			F1 = "󱊫",
+			F2 = "󱊬",
+			F3 = "󱊭",
+			F4 = "󱊮",
+			F5 = "󱊯",
+			F6 = "󱊰",
+			F7 = "󱊱",
+			F8 = "󱊲",
+			F9 = "󱊳",
+			F10 = "󱊴",
+			F11 = "󱊵",
+			F12 = "󱊶",
 		},
 		layout = {
 			height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -56,29 +66,21 @@ return {
 			spacing = 3, -- spacing between columns
 			align = "left", -- align columns left, center or right
 		},
-		ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-		hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
+		win = {
+			no_overlap = true, -- don't allow the popup to overlap with the cursor
+			padding = { 0, 0 },
+			title = true,
+			title_pos = "center",
+			zindex = 1000,
+			-- Additional vim.wo and vim.bo options
+			bo = {},
+			wo = {
+				-- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+			},
+		},
+		-- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
 		show_help = true, -- show a help message in the command line for using WhichKey
 		show_keys = true, -- show the currently pressed key and its label as a message in the command line
-		triggers = "auto", -- automatically setup triggers
-		-- triggers = {"<leader>"} -- or specifiy a list manually
-		-- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
-		triggers_nowait = {
-			-- marks
-			"`",
-			"'",
-			"g`",
-			"g'",
-			-- registers
-			'"',
-			"<c-r>",
-			-- spelling
-			"z=",
-		},
-		triggers_blacklist = {
-			i = { "j", "k" },
-			v = { "j", "k" },
-		},
 		disable = {
 			buftypes = {},
 			filetypes = {},
