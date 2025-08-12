8487ddc18363b5a5e70b62bbe6b5275a4dd1a5df
enfmarinho <eduardonestormarinho228@gmail.com>
2025-08-12 01:02:45 -0300

    Fixing cpp keybindings

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,6 +1,12 @@
 vim.keymap.set(
 	"n",
 	"<leader>lc",
-	"<cmd>ClangdSwitchSourceHeader<cr>",
+	"<cmd>LspClangdSwitchSourceHeader<cr>",
+	{ desc = "Change between header and source file" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>li",
+	"<cmd>LspClangdShowSymbolInfo<cr>",
 	{ desc = "Change between header and source file" }
 )
